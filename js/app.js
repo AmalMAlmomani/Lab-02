4b5f2207f28a80a73a7073357acbbf7bfd186a96
AmalMAlmomani <amalalmomani19@gmail.com>
2020-03-31 16:45:47 +0300

    done

--- js/app.js
+++ js/app.js
@@ -1,60 +1,63 @@
 `use strict`;
- let keywors = [];
-$(document).ready(function() {
-  
-   
-function Horns(horns){
+let allHorns = [];
+let KeywordArray = [];
+
+$(document).ready(function () {
+
+
+  function Horns(horns) {
     this.image_url = horns.image_url;
     this.title = horns.title;
     this.description = horns.description;
     this.keyword = horns.keyword;
     this.numHornes = horns.numHornes;
-    Horns.all.push(this);
-}
- 
-  Horns.all= [];
-  console.log(Horns.all);
+    allHorns.push(this);
+  }
 
-Horns.prototype.render = function(){
+
+
+  Horns.prototype.render = function () {
     let $hornClone = $("#photo-template").clone();
+    $hornClone.find("img").attr("src", this.image_url);
     $hornClone.find("h2").text(this.title);
-     $hornClone.find("p").text(this.description);
-    $hornClone.find("img").attr("class",this.keyword);
-    $hornClone.find("img").attr("src",this.image_url);
-    $hornClone.find("img").attr("alt",this.title);
+    $hornClone.find("p").text(this.description);
+    $hornClone.find("img").attr("class", this.keyword);
+    $hornClone.find("img").attr("alt", this.title);
     $hornClone.removeAttr("id");
-    $hornClone.attr("id", this.keyword);
-    $("main section ").append($hornClone);
-};
-
-Horns.prototype.descriptions = function(){
-    for(let i=0;i < Horns.all.length ; i++){
-
-    }
-
-};
-// function matchKeyword(){
-//     imgArray.forEach(img =>{
-//         if(!KeywordArray .includes(this.keyword)){
-//             KeywordArray.push(this.keyword);
-        
-//     }
-//     });
-// } 
- $("#select").click(function(){
-    $('option').show();
- const readJson = () => {
-    $.ajax("data/page-1.json", { method: "GET", dataType: "JSON" }).
-    then(data => {
-      data.forEach(honrItem => {
-        let horn = new Horns(honrItem);
-        horn.render();
-      });
-    });
+    $hornClone.attr("class", this.keyword);
+    $(" main ").append($hornClone);
   };
-readJson();
-});
- });
+
+  Horns.prototype.selectedMenu = function () {
+    let menuSelect = $('.select');
+    if (!(KeywordArray.includes(this.keyword))) {
+      KeywordArray.push(this.keyword);
+      menuSelect.append(`<option> ${this.keyword} </option>`);
+    }
+  };
+    const readJson = () => {
+      $.ajax("data/page-1.json", { method: "GET", dataType: "JSON" }).
+        then(data => {
+          data.forEach(honrItem => {
+            let horn = new Horns(honrItem);
+            horn.selectedMenu();
+            horn.render();
+          });
+        });
+    };
+    readJson();
+  });
+
+  $('.select').change(function(){
+    var key = $(this).children('option:selected').val();
+    KeywordArray.forEach(function(val){
+      if(key === val){
+        $('div').hide();
+        $(`.${val}`).show();
+      }
+    });
+  });
+
 
 
 
